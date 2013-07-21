1ba266312e8658ba29c6cc4e40ab09d169d6604f
PalmerMatt <Palmer@xakep.ru>
2013-07-22 01:00:20 +0300

    Обработка клика на кнопку
    Доработана кнопка.

--- Claus/src/com/example/claus/MainActivity.java
+++ Claus/src/com/example/claus/MainActivity.java
@@ -3,30 +3,27 @@ package com.example.claus;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	
-	/** Called when the user clicks the Send button */
-	public void sendMessage(View view) {
-	    Intent intent = new Intent(this, DisplayMessageActivity.class);
-	   	    startActivity(intent);
+	public void onClick(View btn) {
+		if (btn.equals(this.findViewById(R.id.button1))) {
+			Intent i = new Intent(this, DisplayMessageActivity.class);
+			startActivity(i);
+			Log.i("CLICK", "Button 1 was clicked");
+		}
 	}
 
 }
