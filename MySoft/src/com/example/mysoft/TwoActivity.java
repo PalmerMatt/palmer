e3128b7df8ebaac41e0b4c4d7515d82c4d577dfe
PalmerMatt <Palmer@xakep.ru>
2013-07-26 13:05:06 +0300

    доработал счетчик во второй активити
    Сделал на второй Активити рабочий счетчик

--- MySoft/src/com/example/mysoft/TwoActivity.java
+++ MySoft/src/com/example/mysoft/TwoActivity.java
@@ -4,23 +4,39 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TwoActivity extends Activity {
-
+	Button button2;
+	int count=0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_two);
+		Log.i("set", "button2");
+		button2 = (Button)findViewById(R.id.button2);
+
+button2.setOnClickListener(new OnClickListener() {
+	@Override
+	public void onClick(View v) {
+		if(v.equals(findViewById(R.id.button2))){
+			Log.i("click", "button2");
+		TextView vw =(TextView)findViewById(R.id.textView1);
+		vw.setText("Click button " + ++count );
+	}}
+});
 	}
+
 	
 	public void sMessage(View v) {
 	Log.i("Click", "Click button1");
 	Intent i = new Intent(this, MainActivity.class);
 	startActivity(i);
-	}
-
-
+	}	
+	
+	
 }
